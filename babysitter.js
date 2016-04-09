666eac39ea6ca04e52b8f797ce9ca081910c64d7
Lovinder Pnag <lspnag@gmail.com>
2016-04-08 22:39:19 -0400

    valid schedule

--- babysitter.js
+++ babysitter.js
@@ -1,3 +1,34 @@
 var BabySitter = function(startTime, endTime) {
+  var valid = false;
+
+  this.schedule = function() {
+    if (startTime >= 17) {
+      valid = true;
+    }
+    return valid;
+  };
+
+  this.valid = function() { return valid; }
+
+};
+
+var GuessingGame = function(answer) {
+  var isSolved = false;
+
+  this.guess = function(guess) {
+    if (guess === answer) {
+      isSolved = true;
+      return 'correct';
+    }
+    else if (guess > answer) {
+      isSolved = false;
+      return 'high';
+    }
+    else {
+      isSolved = false;
+      return 'low';};
+  }
+
+  this.isSolved = function() {return isSolved;}
 
 };
--- babysitter_spec.js
+++ babysitter_spec.js
@@ -2,6 +2,93 @@ require('./spec_helper.js');
 include('babysitter.js');
 
 describe("BabySitter", function() {
-  
+  var sitting;
 
-})
+  var setSchedule = function(x, y) {
+    sitting = new BabySitter(x, y);
+  };
+
+  describe("schedule()", function() {
+    it("is defined", function() {
+      setSchedule(17, 28);
+      expect(sitting.schedule).toBeDefined();
+    });
+
+    it("expects a two arguments", function() {
+      expect(BabySitter.length).toBe(2);
+    });
+
+    it("schedule begins no earlier than 5PM", function() {
+      setSchedule(17, 28);
+      expect(sitting.schedule()).toBe(true);
+    });
+
+  });
+});
+
+
+
+
+
+xdescribe("GuessingGame", function() {
+  var game;
+
+  beforeEach(function() {
+    game = new GuessingGame(10);
+  });
+
+  describe("guess()", function() {
+    it("is defined", function() {
+      expect(game.guess).toBeDefined();
+    });
+
+    it("expects a single argument", function() {
+      expect(GuessingGame.length).toBe(1);
+    });
+
+    it("returns 'high' when the guess is too high", function() {
+      expect(game.guess(100)).toEqual('high');
+    });
+
+    it("returns 'low' when the guess is too low", function() {
+      expect(game.guess(0)).toEqual('low');
+    });
+
+    it("returns 'correct' when the guess is correct", function() {
+      expect(game.guess(10)).toEqual('correct');
+    });
+
+    it("changes isSolved() when a correct guess is made", function() {
+      expect(game.isSolved()).toBe(false);
+      game.guess(10);
+      expect(game.isSolved()).toBe(true);
+    });
+
+    it("doesn't change isSolved() when an incorrect guess is made", function() {
+      expect(game.isSolved()).toBe(false);
+      game.guess(5);
+      expect(game.isSolved()).toBe(false);
+    });
+
+    it("reflects the last guess", function() {
+      game.guess(10);
+      expect(game.isSolved()).toBe(true);
+      game.guess(5);
+      expect(game.isSolved()).toBe(false);
+    });
+  });
+
+  describe("isSolved()", function() {
+    it("is defined", function() {
+      expect(game.isSolved).toBeDefined();
+    });
+
+    it("expects no arguments", function() {
+      expect(game.isSolved.length).toBe(0);
+    });
+
+    it("returns false before a guess is made", function() {
+      expect(game.isSolved()).toBe(false);
+    });
+  });
+});
